a0468ffd6575bf6bb9a3f15ce9fde49a627412fd
Ilma <ilma200503@gmail.com>
2026-05-28 21:18:00 +0200

    Added comment on homepage, added design_pattern.md and doc folder

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+// HomePage handles quiz settings and navigation to quiz
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import styles from './HomePage.module.css'
