16f80145be7a0266ccfceab891f03fadad865ee9
c-g-j <juhlincarlgunnar@gmail.com>
2026-05-28 05:06:48 +0000

    added percentage and score functionality functionality

--- src/pages/ResultsPage.jsx
+++ src/pages/ResultsPage.jsx
@@ -3,13 +3,19 @@ import styles from './ResultsPage.module.css'
 
 
 function ResultsPage() { 
-
+    const location = useLocation()
+    const navigate = useNavigate()
+    const { score, total } = location.state || { score: 25, total: 100 } // default values if no state is passed
+    
+    const percentage = Math.round((score / total) * 100)
     //return the results page 
     return (
     <main>
         <h1 className={styles.test}>Results</h1>
         <div>
             <p>Here are your results!</p>
+            <p>You scored {score} out of {total}.</p>
+            <p>{percentage}%</p>
         </div>
     </main>)
 }  
